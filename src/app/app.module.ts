9b131187a99c08ca96cf595b7cf6ea699d19ba8f
Annie Huang <annie.hn@gmail.com>
2018-12-03 14:01:56 +1100

    Test "npm install ea-c-l-test" package and add button in to html. Success.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'ea-c-l-test/primeng';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
