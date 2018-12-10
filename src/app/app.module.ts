d2513a5ecb4e69a4663b098561dee4ad13797fd6
Annie Huang <annie.hn@gmail.com>
2018-12-10 21:35:06 +1100

    Add <enl-foo> element to the app to test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ExampleNg6LibModule } from 'example-ng6-lib';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ExampleNg6LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
