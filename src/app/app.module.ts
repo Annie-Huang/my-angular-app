78c95554fba553fe249286c4e5b9363073251d2b
Annie Huang <annie.hn@gmail.com>
2018-12-12 21:51:40 +1100

    Test install ea-comp-lib with button.scss applied (e.g. remove eauicomp.min.css). Success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {ButtonModule} from 'ea-c-l-test/eauicomp';
+import {ButtonModule} from 'ea-comp-lib';
 
 @NgModule({
   declarations: [
