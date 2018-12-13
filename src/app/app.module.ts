26d80feeaf4e8fd96f5fef4b2e1792ce3f9d3205
Annie Huang <annie.hn@gmail.com>
2018-12-13 16:35:55 +1100

    Test change from ea-comp-lib to ea-ui. Success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {ButtonModule, FooModule} from 'ea-comp-lib';
+import {ButtonModule, FooModule} from 'ea-ui';
 
 @NgModule({
   declarations: [
