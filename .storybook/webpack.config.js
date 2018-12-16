0216d81c8f388f9c63c9b54c390e9501138ee07a
Annie Huang <annie.hn@gmail.com>
2018-12-16 23:20:23 +1100

    Add webpack, sass-loader

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,42 @@
+// const path = require("path");
+//
+// module.exports = {
+//   module: {
+//     rules: [{
+//       test: /\.less$/,
+//       loaders: ["style-loader", "css-loader", "less-loader"],
+//       include: path.resolve(__dirname, "../")
+//     }]
+//   }
+// };
+
+// const path = require('path');
+// module.exports = {
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.scss$/,
+//         loaders: ["style", "css", "sass"],
+//         include: path.resolve(__dirname, '../')
+//       }
+//     ]
+//   }
+// }
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        {
+          loader: "sass-loader", // compiles Sass to CSS, using Node Sass by default
+          options: {
+            includePaths: ["../"]
+          }
+        }
+      ]
+    }]
+  }
+};
