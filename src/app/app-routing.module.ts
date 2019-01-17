32ef920873972f5943da87a88d5c554a3e01030f
Annie Huang <annie.hn@gmail.com>
2019-01-17 13:20:18 +1100

    Move typography content into the typography component and create router for it..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TypographyTestComponent} from './typography-test/typography-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'typographic', component: TypographyTestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
