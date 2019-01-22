6ecb4f21ff7fd069094beeb08dd9a0692f1c5f99
Annie Huang <annie.hn@gmail.com>
2019-01-22 21:57:38 +1100

    Finally make intellij recognise the cy command by upgrading intellij version. Intellij 2018.2 got problem of use typescript language service. Not sure if that is the problem that causes cypress couldn't recognise typescript.

--- cypress/integration/examples/spec.ts
+++ cypress/integration/examples/spec.ts
@@ -4,7 +4,6 @@ it('loads', () => {
   cy.get('app-root h1').contains('Welcome to my-angular-app!');
 });
 
-
 // cypress/integration/spec.ts
 import { navigateToMyAngularApp, getGreeting } from '../../support/po';
 
@@ -12,6 +11,10 @@ describe('Hello Angular', () => {
   beforeEach(navigateToMyAngularApp);
 
   it('should display welcome message', () => {
-    getGreeting().contains('Welcome to app!');
+    getGreeting().contains('Welcome to my-angular-app!');
+  });
+
+  it('has 3 links', () => {
+    cy.get('app-root li a').should('have.length', 3);
   });
 });
