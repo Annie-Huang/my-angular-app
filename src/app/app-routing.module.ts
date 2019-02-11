f6f04784a5ed4c13c2b6463db9690c3b2a2ae6f9
Annie Huang <annie.hn@gmail.com>
2019-02-11 11:53:30 +1100

    format app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { TextfieldTestComponent } from './textfield-test/textfield-test.component';
-import { TypographyTestComponent}  from './typography-test/typography-test.component';
+import { TypographyTestComponent } from './typography-test/typography-test.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
