75d8e385b7c682cf2eabaf73d5a51a2084d8f492
Annie Huang <annie.hn@gmail.com>
2019-02-28 22:14:15 +1100

    Try to add another test.

--- testcafe-suite/textfield.spec.ts
+++ testcafe-suite/textfield.spec.ts
@@ -3,11 +3,23 @@ import { Selector } from 'testcafe';
 fixture('Getting Started')
   .page('http://localhost:4200/#/textfield');
 
-test('My first test', async t => {
+test('Check header', async t => {
   const header = Selector('.feature-title');
 
   await t
     // .expect(header.value).eql('Textfield');
     .expect(header.innerText).eql('Textfield');
+
+});
+
+test('Accepts input', async t => {
+  const typedName = 'my account name';
+  const typedPostcode = '3000';
+
+  const name = Selector('#account-name-field');
+
+  await t
+    .typeText(name, typedName)
+    .expect(name.innerText).eql('my account name');
 
 });
