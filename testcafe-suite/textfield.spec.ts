ee53a0de59146764146f1d1424c87028f4cb4681
Annie Huang <annie.hn@gmail.com>
2019-03-02 22:06:20 +1100

    Finished textfield testing.

--- testcafe-suite/textfield.spec.ts
+++ testcafe-suite/textfield.spec.ts
@@ -17,8 +17,57 @@ test('Accepts input', async t => {
   const typedPostcode = '3000';
 
   const name = Selector('#account-name-field');
+  const postcode = Selector('#postcode-field');
 
   await t
     .typeText(name, typedName)
-    .expect(name.value).eql('my account name');
+    .expect(name.value).eql(typedName)
+    .typeText(postcode, typedPostcode)
+    .expect(postcode.value).eql(typedPostcode);
+});
+
+
+test('Submit without name and postcode', async t => {
+  const typedName = 'my account name';
+  const typedPostcode = '3000';
+
+  const name = Selector('#account-name-field');
+  const postcode = Selector('#postcode-field');
+  const submitButton = Selector('button').withText('Submit');
+  // const msg = Selector('p').withText('Yay! This form as been submitted!');
+  // const msgExists = Selector('p').withText('Yay! This form as been submitted!').exists;
+
+  await t
+    .typeText(name, typedName)
+    .expect(name.value).eql(typedName)
+    .typeText(postcode, typedPostcode)
+    .expect(postcode.value).eql(typedPostcode)
+    .click(submitButton)
+    .expect(Selector('p').withText('Yay! This form as been submitted!').exists).ok()
+    .expect( Selector('eui-field-errors').exists).notOk()
+    // .expect(msg.visible);
+    // .expect( Selector('form').find('p').nth(3)).eql('Yay! This form as been submitted!', 'check element text', { timeout: 500 })
+    // .expect(Selector('p').innerText).eql('text', 'check element text', { timeout: 500 });
+  ;
+});
+
+test('Submit without name and postcode', async t => {
+  const typedName = 'my account name';
+  const typedPostcode = '3000';
+
+  const name = Selector('#account-name-field');
+  const postcode = Selector('#postcode-field');
+  const submitButton = Selector('button').withText('Submit');
+  // const msg = Selector('p').withText('Yay! This form as been submitted!');
+  // const msgExists = Selector('p').withText('Yay! This form as been submitted!').exists;
+
+  await t
+    .click(submitButton)
+    .expect( Selector('eui-field-errors').find('div').withText('Please enter a name').exists).ok()
+    .expect( Selector('eui-field-errors').find('div').withText('Please enter a postcode').exists).ok()
+    // .expect(Selector('p').withText('Yay! This form as been submitted!').exists).ok()
+  // .expect(msg.visible);
+  // .expect( Selector('form').find('p').nth(3)).eql('Yay! This form as been submitted!', 'check element text', { timeout: 500 })
+  // .expect(Selector('p').innerText).eql('text', 'check element text', { timeout: 500 });
+  ;
 });
