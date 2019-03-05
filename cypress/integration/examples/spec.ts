e4d7063bf82c20e9408becbf8a187b68757435c9
Annie Huang <annie.hn@gmail.com>
2019-03-05 15:06:52 +1100

    update cypress testing script

--- cypress/integration/examples/spec.ts
+++ cypress/integration/examples/spec.ts
@@ -1,21 +1,21 @@
-// cypress/integration/spec.ts
-it('loads', () => {
-  // cy.visit('http://localhost:4201');
-  cy.visit('');
-  cy.get('app-root h1').contains('Welcome to my-angular-app!');
-});
-
-// cypress/integration/spec.ts
-import { navigateToMyAngularApp, getGreeting } from '../../support/po';
-
-describe('Hello Angular', () => {
-  beforeEach(navigateToMyAngularApp);
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to my-angular-app!');
-  });
-
-  it('has 3 links', () => {
-    cy.get('app-root li a').should('have.length', 3);
-  });
-});
+// // cypress/integration/spec.ts
+// it('loads', () => {
+//   // cy.visit('http://localhost:4201');
+//   cy.visit('');
+//   cy.get('app-root h1').contains('Welcome to my-angular-app!');
+// });
+//
+// // cypress/integration/spec.ts
+// import { navigateToMyAngularApp, getGreeting } from '../../support/po';
+//
+// describe('Hello Angular', () => {
+//   beforeEach(navigateToMyAngularApp);
+//
+//   it('should display welcome message', () => {
+//     getGreeting().contains('Welcome to my-angular-app!');
+//   });
+//
+//   it('has 3 links', () => {
+//     cy.get('app-root li a').should('have.length', 3);
+//   });
+// });
