8d6a4ae4df9c140a75bad5354c647c702912ab49
Annie Huang <annie.hn@gmail.com>
2019-04-04 10:20:41 +1100

    FieldTest1. Remove unnecessary files.

--- src/app/fielderror1-test/fielderror1-test.component.ts
+++ src/app/fielderror1-test/fielderror1-test.component.ts
@@ -8,7 +8,7 @@ import {EuiFormDirective} from '@ea/ea-ui';
   templateUrl: './fielderror1-test.component.html',
   styleUrls: ['./fielderror1-test.component.scss']
 })
-export class Fielderror1TestComponent implements OnInit, AfterViewInit {
+export class Fielderror1TestComponent implements OnInit {
   readonly successMessage = 'Yay! This form as been submitted!';
   @ViewChildren(EuiFormDirective) euiFormDirectives: QueryList<EuiFormDirective>;
 
@@ -19,35 +19,9 @@ export class Fielderror1TestComponent implements OnInit, AfterViewInit {
   form1Values: object;
   form1Status: string;
 
-  form2AccountNumber: NgModel;
-  form2Postcode: NgModel;
-  form2EuiFormDirective: EuiFormDirective;
-  form2Success = '';
-  form2Values: object;
-  form2Status: string;
-
-  form3: FormGroup;
-  form3ErrorVisible = false;
-  form3Success = '';
-
-  form4: FormGroup;
-  form4EuiFormDirective: EuiFormDirective;
-  form4Success = '';
-
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.createForms();
-  }
-
-  ngAfterViewInit() {
-    this.euiFormDirectives.forEach((item, index) => {
-      if (index === 0) {
-        this.form2EuiFormDirective = item;
-      } else if (index === 1) {
-        this.form4EuiFormDirective = item;
-      }
-    });
   }
 
   onForm1Submit(form: NgForm) {
@@ -65,62 +39,6 @@ export class Fielderror1TestComponent implements OnInit, AfterViewInit {
   onForm1Reset(form: NgForm) {
     form.reset();
     this.form1ErrorVisible = false;
-  }
-
-  onForm2Submit(form: NgForm): void {
-    this.form2Values = form.value;
-    this.form2Status = form.status;
-
-    if (form.valid) {
-      this.form2Success = this.successMessage;
-    } else {
-      this.form2Success = '';
-    }
-  }
-
-  onForm2Reset(form: NgForm) {
-    form.reset();
-    this.form2EuiFormDirective.onReset();
-  }
-
-  createForms() {
-    this.form3 = this.fb.group({
-      form3AccountName: [null, [Validators.required]],
-      form3Postcode: [null, [Validators.required, Validators.pattern(/^[0-9]{4}$/)]]
-    });
-
-    this.form4 = this.fb.group({
-      form4AccountName: [null, [Validators.required]],
-      form4Postcode: [null, [Validators.required, Validators.pattern(/^[0-9]{4}$/)]]
-    });
-  }
-
-  onForm3Submit(): void {
-    this.form3ErrorVisible = true;
-
-    if (this.form3.valid) {
-      this.form3Success = this.successMessage;
-    } else {
-      this.form3Success = '';
-    }
-  }
-
-  onForm3Reset() {
-    this.form3.reset();
-    this.form3ErrorVisible = false;
-  }
-
-  onForm4Submit(): void {
-    if (this.form4.valid) {
-      this.form4Success = this.successMessage;
-    } else {
-      this.form4Success = '';
-    }
-  }
-
-  onForm4Reset() {
-    this.form4.reset();
-    this.form4EuiFormDirective.onReset();
   }
 
 }
